44a584a8cf29792d9a7b344a487071bd345dece4
Surajmal Kumar <surajkumar7252@gmail.com>
2020-10-08 12:23:34 +0530

    [Surajmal Kumar].Refactoring use case 4. Use case 4 to feed custom value to desired index success.

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -9,18 +9,13 @@ public class TicTacToeGame {
 	static Scanner sc=new Scanner(System.in);
 	public static char[] createBoard() {
 		 char[] Board= new char[10];
-	     for(int index=0;index<10;index++){
+	     for(int index=1;index<10;index++){
 	    	Board[index]=' ';
         	}
 	     return Board;
 	  }
 	
-	public static void showBoard() {
-		 char[] Board= new char[10];
-	     for(int index=0;index<10;index++){
-	    	Board[index]=' ';
-       	}
-	  }
+	
 	
 	public static char choice() {
 		System.out.println("Enter you input(X/O):");
@@ -44,17 +39,63 @@ public class TicTacToeGame {
 	
 	public static void showBoard(char[] assignedBoard)
 	{
-	 for(int index=0;index<10;index++)
-	 {
-		 System.out.println(assignedBoard[index]+ "--");
+	 for(int row=1;row<=3;row++){
+		 for(int column=0;column<=3;column++) {
+			 System.out.print(assignedBoard[row+column]+ "--"); 
+		 }
+		 
+		 System.out.println();
 	 }
 	}
+
+	public static boolean move(int index,char[] assignedBoard)
+	{     boolean fact;
+	      char[] Board= new char[10];
+           Board=assignedBoard;
+           if(Board[index]!=' ')
+           {
+        	  return false; 
+           }
+           else
+           {
+        	   return true;
+           }
+		 
+	}
+	
+	public static char[] userDefinedInput(char[] assignedBoard)
+	{   char[] Board= new char[10];
+	     Board=assignedBoard;
+	     char choice;
+	     boolean goAhead;
+	     do
+	     {
+		System.out.println("Enter your desired index:(between 1 to 9) " );
+		int desiredIndex = sc.nextInt();
+		goAhead=move(desiredIndex,assignedBoard);
+		if(goAhead){
+			System.out.println("Your selected index in vacant. You can mark here");
+			System.out.println("enter you input (X/O");
+			char desiredInput = sc.next().charAt(0);
+			Board[desiredIndex]=desiredInput;
+		 }
+		else
+		{
+			System.out.println("index not available");
+		}
+		System.out.println("do you want to perform again (Y/N:");
+		choice = sc.next().charAt(0);
+	     }while(choice=='Y');
+		return Board;
+	}
+	
      public static void main(String[] args) {
 	    char[] assignedBoard = new char[10];
 	    assignedBoard=createBoard();
 	    showBoard(assignedBoard);
 	    char optedChoice=choice();
-	    
+	    assignedBoard= userDefinedInput(assignedBoard);
+	    showBoard(assignedBoard);
 		
 	    System.out.println("chosen option "+ optedChoice);
 		}
