f26e5ad948b4e008db9092d0a33e2586f4596b39
Surajmal Kumar <surajkumar7252@gmail.com>
2020-10-08 11:20:54 +0530

    [Surajmal Kumar].adding use case 3. Use case 3 to  show board  success.

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,24 +1,39 @@
 import java.util.Scanner;
 
 public class TicTacToeGame {
+	
+	public static final char EMPTY =' ';
+	public static final char X_CHARACTER ='X';
+	public static final char O_CHARACTER ='O';
+	
 	static Scanner sc=new Scanner(System.in);
-	public static void createBoard() {
+	public static char[] createBoard() {
 		 char[] Board= new char[10];
 	     for(int index=0;index<10;index++){
 	    	Board[index]=' ';
         	}
+	     return Board;
+	  }
+	
+	public static void showBoard() {
+		 char[] Board= new char[10];
+	     for(int index=0;index<10;index++){
+	    	Board[index]=' ';
+       	}
 	  }
 	
 	public static char choice() {
 		System.out.println("Enter you input(X/O):");
 		char option = sc.next().charAt(0); 
+		char computerOption;
 		if(option=='X')
 		{
-			System.out.println("you chose X");
+			computerOption= X_CHARACTER;
+			
 		}
 		if(option=='O')
 		{
-			System.out.println("you chose X");
+			computerOption= O_CHARACTER;
 		}
 		else
 		{
@@ -26,10 +41,21 @@ public class TicTacToeGame {
 		}
 		return option;
 	}
+	
+	public static void showBoard(char[] assignedBoard)
+	{
+	 for(int index=0;index<10;index++)
+	 {
+		 System.out.println(assignedBoard[index]+ "--");
+	 }
+	}
      public static void main(String[] args) {
-	    createBoard();
+	    char[] assignedBoard = new char[10];
+	    assignedBoard=createBoard();
+	    showBoard(assignedBoard);
 	    char optedChoice=choice();
 	    
+		
 	    System.out.println("chosen option "+ optedChoice);
 		}
     }
