9a8644efad0273055381cd675af913541ec21c01
Surajmal Kumar <surajkumar7252@gmail.com>
2020-10-08 10:42:49 +0530

    [Surajmal Kumar].adding use case 2. Use case 2 to takein any user input success.

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,14 +1,35 @@
- import java.util.Scanner;
+import java.util.Scanner;
 
 public class TicTacToeGame {
-  
+	static Scanner sc=new Scanner(System.in);
 	public static void createBoard() {
 		 char[] Board= new char[10];
 	     for(int index=0;index<10;index++){
 	    	Board[index]=' ';
         	}
 	  }
+	
+	public static char choice() {
+		System.out.println("Enter you input(X/O):");
+		char option = sc.next().charAt(0); 
+		if(option=='X')
+		{
+			System.out.println("you chose X");
+		}
+		if(option=='O')
+		{
+			System.out.println("you chose X");
+		}
+		else
+		{
+			System.out.println("Please enter the valid character");
+		}
+		return option;
+	}
      public static void main(String[] args) {
 	    createBoard();
+	    char optedChoice=choice();
+	    
+	    System.out.println("chosen option "+ optedChoice);
 		}
     }
